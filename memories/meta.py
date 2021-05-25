cf05ae8d8edc64a84991967f6af1c831ec932829
veedata <viraj.dt@somaiya.edu>
2021-05-25 23:43:24 +0530

    time can also be entered & bug fixes

--- memories/meta.py
+++ memories/meta.py
@@ -2,19 +2,19 @@ import os
 import piexif
 from datetime import datetime
 
-def addDate(imageInputPath: str, newDate: str) -> None:
+def addDate(imageInputPath: str, newDateTime: str) -> None:
     """Add date when the image was originally taken
 
     @type imageInputPath: str
     @param imageInputPath: The path of the input image is to be passed
-    @type newDate: str
-    @param newDate: Date in the format "day/month/year"
+    @type newDateTime: str
+    @param newDateTime: Date in the format "day/month/year hours:mins:secs"
     """
 
     try:
         exif_dict = piexif.load(imageInputPath)
         
-        newExifDate = datetime.strptime(newdate, '%d/%m/%Y')
+        newExifDate = datetime.strptime(newDateTime, '%d/%m/%Y %H:%M:%S')
         newExifDate = newExifDate.strftime('%Y:%m:%d %H:%M:%S')
         
         exifDateToday = datetime.today()
@@ -24,21 +24,21 @@ def addDate(imageInputPath: str, newDate: str) -> None:
         exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = exifDateToday
         
         exif_bytes = piexif.dump(exif_dict)
-        piexif.insert(exif_bytes, imagePath)
+        piexif.insert(exif_bytes, imageInputPath)
 
     except:
         pass
 
-def bulkAddDate(folderPath: str, newDate: str) -> None:
+def bulkAddDate(folderPath: str, newDateTime: str) -> None:
     """Add date to all images in a folder
 
     @type folderPath: str
     @param folderPath: The path of folder
-    @type newDate: str
-    @param newDate: Date in the format "day/month/year"
+    @type newDateTime: str
+    @param newDateTime: Date in the format "day/month/year hours:mins:secs"
     """
 
     for (dirpath, dirnames, filenames) in os.walk(folderPath):
         for eachFile in filenames:
             imagePath = os.path.join(dirpath, eachFile)
-            addDate(imagePath, newDate)
+            addDate(imagePath, newDateTime)
