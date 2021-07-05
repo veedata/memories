28864bc3c4939d83cea2d1c4204286ab75cbef51
veedata <viraj.dt@somaiya.edu>
2021-07-05 21:40:38 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = 'memories',
-    version = '0.5',
+    version = '0.6',
     url = 'https://github.com/veedata/memories',
-    download_url = 'https://github.com/veedata/memories/archive/refs/tags/v0.5-beta.tar.gz',
+    download_url = 'https://github.com/veedata/memories/archive/refs/tags/v0.6-beta.tar.gz',
     author = 'Viraj Thakkar',
     author_email = 'vdthakkar111@gmail.com',
     description = 'A library for those who want to convert their older images into digitised format (with metadata), and beautify them using borders and other options.',
