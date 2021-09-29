0303766a6437c66036c2e6c9a194cec8f0f05cc8
veedata <viraj.dt@somaiya.edu>
2021-09-29 16:41:26 +0530

    border fixes

--- memories/border.py
+++ memories/border.py
@@ -15,7 +15,7 @@ def make_border(input_image: np.ndarray,
     :type input_image: np.ndarray
     :param border_type: Select border type - normal, curved (default is normal)
     :type border_type: str
-    :param bgr_value: The BGR value of the background in a list
+    :param bgr_value: The BGR value of the background in a list ([B, G, R, A])
     :type bgr_value: list, optional
     :param border_dimension: The value (in pixels) of the border to be made,
      order is top, bottom, left, right. Default is 1% of max(height, width)
@@ -56,11 +56,6 @@ def make_border(input_image: np.ndarray,
                                    cv2.BORDER_CONSTANT,
                                    value=(255, 255, 255, 0))
 
-        # Need to add error checking conditions!
-        bgr_value_opaque = copy.deepcopy(bgr_value)
-        if len(bgr_value) == 3:
-            bgr_value_opaque.append(255)
-
         top_left = (0, 0)
         top_right = (image.shape[1], 0)
         bottom_right = (image.shape[1], image.shape[0])
@@ -72,52 +67,52 @@ def make_border(input_image: np.ndarray,
             (top_left[0] + radius_dimension[0] + border_dimension[0] // 2,
              top_left[1] + border_dimension[0] // 2),
             (top_right[0] - radius_dimension[1] - border_dimension[0] // 2,
-             top_right[1] + border_dimension[0] // 2), bgr_value_opaque,
+             top_right[1] + border_dimension[0] // 2), bgr_value,
             abs(border_dimension[0]), cv2.LINE_AA)
         cv2.line(image,
                  (top_right[0] - border_dimension[0] // 2, top_right[1] +
                   radius_dimension[1] + border_dimension[0] // 2),
                  (bottom_right[0] - border_dimension[0] // 2, bottom_right[1] -
-                  radius_dimension[2] - border_dimension[0] // 2),
-                 bgr_value_opaque, abs(border_dimension[0]), cv2.LINE_AA)
+                  radius_dimension[2] - border_dimension[0] // 2), bgr_value,
+                 abs(border_dimension[0]), cv2.LINE_AA)
         cv2.line(
             image,
             (bottom_right[0] - radius_dimension[2] - border_dimension[0] // 2,
              bottom_left[1] - border_dimension[0] // 2),
             (bottom_left[0] + radius_dimension[3] + border_dimension[0] // 2,
-             bottom_right[1] - border_dimension[0] // 2), bgr_value_opaque,
+             bottom_right[1] - border_dimension[0] // 2), bgr_value,
             abs(border_dimension[0]), cv2.LINE_AA)
         cv2.line(image,
                  (bottom_left[0] + border_dimension[0] // 2, bottom_left[1] -
                   radius_dimension[3] - border_dimension[0] // 2),
                  (top_left[0] + border_dimension[0] // 2, top_left[1] +
-                  radius_dimension[0] + border_dimension[0] // 2),
-                 bgr_value_opaque, abs(border_dimension[0]), cv2.LINE_AA)
+                  radius_dimension[0] + border_dimension[0] // 2), bgr_value,
+                 abs(border_dimension[0]), cv2.LINE_AA)
 
         # arcs
         cv2.ellipse(
             image,
             (top_left[0] + radius_dimension[0] + border_dimension[0] // 2,
              top_left[1] + radius_dimension[0] + border_dimension[0] // 2),
-            (radius_dimension[0], radius_dimension[0]), 180, 0, 90,
-            bgr_value_opaque, border_dimension[0], cv2.LINE_AA)
+            (radius_dimension[0], radius_dimension[0]), 180, 0, 90, bgr_value,
+            border_dimension[0], cv2.LINE_AA)
         cv2.ellipse(
             image,
             (top_right[0] - radius_dimension[1] - border_dimension[0] // 2,
              top_right[1] + radius_dimension[1] + border_dimension[0] // 2),
-            (radius_dimension[1], radius_dimension[1]), 270, 0, 90,
-            bgr_value_opaque, border_dimension[0], cv2.LINE_AA)
+            (radius_dimension[1], radius_dimension[1]), 270, 0, 90, bgr_value,
+            border_dimension[0], cv2.LINE_AA)
         cv2.ellipse(
             image,
             (bottom_right[0] - radius_dimension[2] - border_dimension[0] // 2,
              bottom_right[1] - radius_dimension[2] - border_dimension[0] // 2),
-            (radius_dimension[2], radius_dimension[2]), 0, 0, 90,
-            bgr_value_opaque, border_dimension[0], cv2.LINE_AA)
+            (radius_dimension[2], radius_dimension[2]), 0, 0, 90, bgr_value,
+            border_dimension[0], cv2.LINE_AA)
         cv2.ellipse(
             image,
             (bottom_left[0] + radius_dimension[3] + border_dimension[0] // 2,
              bottom_left[1] - radius_dimension[3] - border_dimension[0] // 2),
-            (radius_dimension[3], radius_dimension[3]), 90, 0, 90,
-            bgr_value_opaque, border_dimension[0], cv2.LINE_AA)
+            (radius_dimension[3], radius_dimension[3]), 90, 0, 90, bgr_value,
+            border_dimension[0], cv2.LINE_AA)
 
     return image
