2a97f1c2f97c9727032b84205e975a2c63403c78
veedata <viraj.dt@somaiya.edu>
2021-12-03 02:58:52 +0530

    updated to version 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2021, Viraj Thakkar'
 author = 'Viraj Thakkar'
 
 # The short X.Y version
-version = '1.0.0'
+version = '1.0.1'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-stable'
+release = '1.0.1-stable'
 
 
 # -- General configuration ---------------------------------------------------
